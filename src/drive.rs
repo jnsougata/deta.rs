d5dea6f7df15916e84757b7e4361856436cfdb48
Emil Thorenfeldt <emt@magenta-aps.dk>
2023-03-01 18:12:17 +0100

    Swap reqwest out for ureq

--- src/drive.rs
+++ src/drive.rs
@@ -1,6 +1,5 @@
 use serde_json::{json, Value};
 
-
 /// Drive is the struct that represents a Deta Drive.
 /// ## Methods
 /// #### `list(prefix, limit, last)`
@@ -21,13 +20,12 @@ pub struct Drive {
 const DRIVE_URL: &str = "https://drive.deta.sh/v1";
 
 impl Drive {
-
     pub fn list(
         &self,
         prefix: Option<&str>,
         limit: Option<i32>,
         last: Option<String>,
-    ) -> Result<Value, reqwest::Error> {
+    ) -> Result<Value, std::io::Error> {
         let mut url = format!("{}/{}/{}/files?", DRIVE_URL, self.project_id, self.name);
         if let Some(limit) = limit {
             url.push_str(&format!("limit={}", limit));
@@ -40,111 +38,109 @@ impl Drive {
         if let Some(last) = last {
             url.push_str(&format!("&last={}", last));
         }
-        let client = reqwest::blocking::Client::new();
-        let resp = client
-            .get(&url)
-            .header("X-Api-Key", &self.project_key)
-            .header("Content-Type", "application/json")
-            .send()
+        let resp = ureq::get(&url)
+            .set("X-Api-Key", &self.project_key)
+            .set("Content-Type", "application/json")
+            .call()
             .unwrap();
-        resp.json::<Value>()
-    }
-    
-    pub fn get(&self, filename: &str) -> Result<Vec<u8>, reqwest::Error> {
-        let url = format!("{}/{}/{}/files/download?name={}", DRIVE_URL, self.project_id, self.name, filename);
-        let client = reqwest::blocking::Client::new();
-        let resp = client
-            .get(&url)
-            .header("X-Api-Key", &self.project_key)
-            .header("Content-Type", "application/json")
-            .send().unwrap();
-        Result::Ok(resp.bytes().unwrap().to_vec())
+        resp.into_json::<Value>()
     }
 
-    pub fn put<'a >(&self, save_as: &str, content: &'a [u8]) -> Result<Value, reqwest::Error> {
+    pub fn get(&self, filename: &str) -> Result<String, std::io::Error> {
+        let url = format!(
+            "{}/{}/{}/files/download?name={}",
+            DRIVE_URL, self.project_id, self.name, filename
+        );
+        let resp = ureq::get(&url)
+            .set("X-Api-Key", &self.project_key)
+            .set("Content-Type", "application/json")
+            .call()
+            .unwrap();
+        resp.into_string()
+    }
+
+    pub fn put<'a>(&self, save_as: &str, content: &'a [u8]) -> Result<Value, std::io::Error> {
         if content.len() <= 10 * 1024 * 1024 {
-            let url = format!("{}/{}/{}/files?name={}", DRIVE_URL, self.project_id, self.name, save_as);
-            let client = reqwest::blocking::Client::new();
-            let resp = client
-                .post(&url)
-                .header("X-Api-Key", &self.project_key)
-                .header("Content-Type", "application/octet-stream")
-                .header("name", save_as)
-                .body(content.to_owned())
-                .send().unwrap();
-            return resp.json::<Value>();
+            let url = format!(
+                "{}/{}/{}/files?name={}",
+                DRIVE_URL, self.project_id, self.name, save_as
+            );
+            let resp = ureq::post(&url)
+                .set("X-Api-Key", &self.project_key)
+                .set("Content-Type", "application/octet-stream")
+                .set("name", save_as)
+                .send_bytes(content)
+                .unwrap();
+            return resp.into_json::<Value>();
         } else {
             const CHUNK_SIZE: usize = 10 * 1024 * 1024;
             let chunks = content.chunks(CHUNK_SIZE);
             let init_url = format!(
-                "{}/{}/{}/uploads?name={}", 
+                "{}/{}/{}/uploads?name={}",
                 DRIVE_URL, self.project_id, self.name, save_as
             );
-            let client = reqwest::blocking::Client::new();
-            let init_resp = client
-                .post(&init_url)
-                .header("X-Api-Key", &self.project_key)
-                .header("Content-Type", "application/octet-stream")
-                .header("name", save_as)
-                .send().unwrap();
-            let init_data = init_resp.json::<Value>().unwrap();
+            let init_resp = ureq::post(&init_url)
+                .set("X-Api-Key", &self.project_key)
+                .set("Content-Type", "application/octet-stream")
+                .set("name", save_as)
+                .call()
+                .unwrap();
+            let init_data = init_resp.into_json::<Value>().unwrap();
             let upload_id = init_data["upload_id"].to_string().replace("\"", "");
             let file_name = init_data["name"].to_string().replace("\"", "");
             let mut done = vec![];
             for (index, chunk) in chunks.enumerate() {
                 let part_url = format!(
-                    "{}/{}/{}/uploads/{}/parts?name={}&part={}", 
-                    DRIVE_URL, self.project_id, self.name, upload_id, file_name, index + 1
+                    "{}/{}/{}/uploads/{}/parts?name={}&part={}",
+                    DRIVE_URL,
+                    self.project_id,
+                    self.name,
+                    upload_id,
+                    file_name,
+                    index + 1
                 );
-                let client = reqwest::blocking::Client::new();
-                let part_resp = client
-                    .post(&part_url)
-                    .header("X-Api-Key", &self.project_key)
-                    .header("Content-Type", "application/octet-stream")
-                    .body(chunk.to_owned())
-                    .send().unwrap();
-                done.push(part_resp.status().as_u16());
+                let part_resp = ureq::post(&part_url)
+                    .set("X-Api-Key", &self.project_key)
+                    .set("Content-Type", "application/octet-stream")
+                    .send_bytes(chunk)
+                    .unwrap();
+                done.push(part_resp.status());
             }
             let success = done.iter().all(|&x| x == 200);
             if success {
                 let complete_url = format!(
-                    "{}/{}/{}/uploads/{}?name={}", 
+                    "{}/{}/{}/uploads/{}?name={}",
                     DRIVE_URL, self.project_id, self.name, upload_id, file_name
                 );
-                let client = reqwest::blocking::Client::new();
-                let complete_resp = client
-                    .patch(&complete_url)
-                    .header("X-Api-Key", &self.project_key)
-                    .header("Content-Type", "application/json")
-                    .send().unwrap();
-                return complete_resp.json::<Value>();
+                let complete_resp = ureq::patch(&complete_url)
+                    .set("X-Api-Key", &self.project_key)
+                    .set("Content-Type", "application/json")
+                    .call()
+                    .unwrap();
+                return complete_resp.into_json::<Value>();
             } else {
                 let abort_url = format!(
-                    "{}/{}/{}/uploads/{}?name={}", 
+                    "{}/{}/{}/uploads/{}?name={}",
                     DRIVE_URL, self.project_id, self.name, upload_id, file_name
                 );
-                let client = reqwest::blocking::Client::new();
-                let abort_resp = client
-                    .delete(&abort_url)
-                    .header("X-Api-Key", &self.project_key)
-                    .header("Content-Type", "application/json")
-                    .send().unwrap();
-                return abort_resp.json::<Value>();
+                let abort_resp = ureq::delete(&abort_url)
+                    .set("X-Api-Key", &self.project_key)
+                    .set("Content-Type", "application/json")
+                    .call()
+                    .unwrap();
+                return abort_resp.into_json::<Value>();
             }
         }
     }
 
-    pub fn delete(&self, names: Vec<&str>) -> Result<Value, reqwest::Error> {
+    pub fn delete(&self, names: Vec<&str>) -> Result<Value, std::io::Error> {
         let url = format!("{}/{}/{}/files", DRIVE_URL, self.project_id, self.name);
-        let payload = json!({"names": names});
-        let client = reqwest::blocking::Client::new();
-        let resp = client
-            .delete(&url)
-            .header("X-Api-Key", &self.project_key)
-            .header("Content-Type", "application/json")
-            .body(payload.to_string())
-            .send().unwrap();
-        resp.json::<Value>()
+        let payload = json!({ "names": names });
+        let resp = ureq::delete(&url)
+            .set("X-Api-Key", &self.project_key)
+            .set("Content-Type", "application/json")
+            .send_json(payload.to_string())
+            .unwrap();
+        resp.into_json::<Value>()
     }
-    
 }
