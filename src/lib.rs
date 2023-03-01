d0220830ac707a2ce6ed40ec918420e2827402d2
Emil Thorenfeldt <emt@magenta-aps.dk>
2023-03-01 18:12:33 +0100

    Derive clone on Deta

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,26 @@
 //! ## Usage
 //! ```rust
 //! use deta_rs;
-//! 
+//!
 //! let deta = deta_rs::new("project_key");
 //! let base = deta.base("base_name");
 //! let drive = deta.drive("drive_name");
-//! 
+//!
 //! let record = base.get("key_here");
 //! let file = drive.get("sample.png");
 //! ```
 
 pub mod base;
-pub mod utils;
 pub mod drive;
+pub mod utils;
 
-
+#[derive(Clone)]
 pub struct Deta {
     pub project_key: String,
     pub project_id: String,
 }
 
 impl Deta {
-    
     pub fn base(&self, name: &str) -> base::Base {
         base::Base {
             name: name.to_string(),
@@ -32,7 +31,7 @@ impl Deta {
         }
     }
 
-    pub fn drive(&self, name: &str) -> drive::Drive{
+    pub fn drive(&self, name: &str) -> drive::Drive {
         drive::Drive {
             name: name.to_string(),
             project_id: self.project_id.clone(),
@@ -42,9 +41,9 @@ impl Deta {
 
     pub fn new(project_key: &str) -> Deta {
         let project_id = project_key.split("_").collect::<Vec<&str>>()[0];
-        Deta { 
-            project_key: project_key.to_string(), 
-            project_id: project_id.to_string()
+        Deta {
+            project_key: project_key.to_string(),
+            project_id: project_id.to_string(),
         }
     }
 }
